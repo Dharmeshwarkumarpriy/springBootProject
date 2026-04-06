b0125258b80798605051553929d7302ba42d87ce
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-04-06 22:04:13 +0530

    save(single, multiple) operation using JPA and spring boot.

--- bootjpaexample/src/main/java/com/jpa/test/BootjpaexampleApplication.java
+++ bootjpaexample/src/main/java/com/jpa/test/BootjpaexampleApplication.java
@@ -24,46 +24,37 @@ public class BootjpaexampleApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(BootjpaexampleApplication.class, args);
 		UserRepository userRepository = context.getBean(UserRepository.class);
-//		User user=new User();
-//		user.setName("dharmy");
-//		user.setCity("dehri");
-//		user.setStatus("i am java programmer");
-//		
-//		User user2 = userRepository.save(user);
-//		System.out.println(user2);
-//		
-//		// create object  of  user...
-//		User user3 =new User();
-//		user3.setName("user3");
-//		user3.setCity("city1");
-//		user3.setStatus("python");
-//		
-//		User user4=new User();
-//		user4.setName("user4");
-//		user4.setCity("city2");
-//		user4.setStatus("java programmer");
-//		
-//		// saving single user...
-//		User resultUser = userRepository.save(user3);
-//		
-//		// saving multiple users...
-//		List<User> users = List.of(user3, user4);
-//		Iterable<User> result = userRepository.saveAll(users);
-//		result.forEach(userR->{
-//			System.out.println(userR);
-//		});
-//		
-//		System.out.println("saved user "+resultUser);
-//		System.out.println("done");
-//		
-		// update the user of id 11....
-		Optional<User> byId = userRepository.findById(102);
-		User user = byId.get();
-		user.setName("golu");
+		User user=new User();
+		user.setName("dharmy");
+		user.setCity("dehri");
+		user.setStatus("i am java programmer");
 		
-		User result = userRepository.save(user);
-		System.out.println(result);
+		User user2 = userRepository.save(user);
+		System.out.println(user2);
 		
+		// create object  of  user...
+		User user3 =new User();
+		user3.setName("user3");
+		user3.setCity("city1");
+		user3.setStatus("python");
+		
+		User user4=new User();
+		user4.setName("user4");
+		user4.setCity("city2");
+		user4.setStatus("java programmer");
+		
+		// saving single user...
+		User resultUser = userRepository.save(user3);
+		
+		// saving multiple users...
+		List<User> users = List.of(user3, user4);
+		Iterable<User> result = userRepository.saveAll(users);
+		result.forEach(userR->{
+			System.out.println(userR);
+		});
+		
+		System.out.println("saved user "+resultUser);
+		System.out.println("done");
 		
 		
 	}
