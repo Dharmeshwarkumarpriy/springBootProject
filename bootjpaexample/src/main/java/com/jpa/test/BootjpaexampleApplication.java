a2906404e41aa3186da50059bfa004976ef3b3d4
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-04-06 21:39:52 +0530

    save(single, multiple) operation using JPA and spring boot.

--- bootjpaexample/src/main/java/com/jpa/test/BootjpaexampleApplication.java
+++ bootjpaexample/src/main/java/com/jpa/test/BootjpaexampleApplication.java
@@ -1,5 +1,7 @@
 package com.jpa.test;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +13,12 @@ import com.jpa.test.entities.User;
 @SpringBootApplication
 public class BootjpaexampleApplication {
 
+	private final UserRepository userRepository;
+
+	BootjpaexampleApplication(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(BootjpaexampleApplication.class, args);
 		UserRepository userRepository = context.getBean(UserRepository.class);
@@ -21,6 +29,31 @@ public class BootjpaexampleApplication {
 		
 		User user2 = userRepository.save(user);
 		System.out.println(user2);
+		
+		// create object  of  user...
+		User user3 =new User();
+		user3.setName("user3");
+		user3.setCity("city1");
+		user3.setStatus("python");
+		
+		User user4=new User();
+		user4.setName("user4");
+		user4.setCity("city2");
+		user4.setStatus("java programmer");
+		
+		// saving single user...
+		User resultUser = userRepository.save(user3);
+		
+		// saving multiple users...
+		List<User> users = List.of(user3, user4);
+		Iterable<User> result = userRepository.saveAll(users);
+		result.forEach(userR->{
+			System.out.println(userR);
+		});
+		
+		System.out.println("saved user "+resultUser);
+		System.out.println("done");
+		
 	}
 
 }
