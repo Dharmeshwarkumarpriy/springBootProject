76f67b4beefff55aabb5b7d4cef698c1faa17f23
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-04-07 22:07:49 +0530

    use spring boot DevTools.

---
+++ bootdevtool/src/main/java/com/dev/test/TestController.java
@@ -0,0 +1,20 @@
+package com.dev.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/test")
+	@ResponseBody
+	public String test() {
+		
+		int a=78;
+		int b=7556;
+		int c=534;
+		return "this is just testing  sum of a and b and c is "+(a+b+c);
+	}
+	
+}
