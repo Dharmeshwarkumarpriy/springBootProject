6e5698cd324e44c4ed30ff90daff7b503b49ae59
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-04-09 14:35:17 +0530

    @PostMapping | Creating REsource REST API | REST API using spring boot.

--- bootrestbook/src/main/java/com/api/book/bootrestbook/controller/BookController.java
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/controller/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -41,4 +43,12 @@ public class BookController {
 		return bookService.getBookById(id);
 	}
 
+	// new book handler...
+	@PostMapping("/books")
+	public Book addBook(@RequestBody Book book) {		
+		Book b=this.bookService.addBook(book);
+		System.out.println(book);
+		return b;
+	}
+	
 }
