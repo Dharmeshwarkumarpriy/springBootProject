8a2c6cf8760f42f3f82becd5f5996459a1fdf944
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-04-14 11:09:41 +0530

    Jackson - Bidirectional Relationships | @JsonManagedReference, @JsonBackReference | Spring Boot.

--- bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Author.java
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Author.java
@@ -1,9 +1,12 @@
 package com.api.book.bootrestbook.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Author {
@@ -14,6 +17,10 @@ public class Author {
 	private String firstName;
 	private String lastName;
 	private String language;
+	
+	@OneToOne(mappedBy = "author")
+	@JsonBackReference
+	private Book book;
 	
 	public Author() {
 		super();
