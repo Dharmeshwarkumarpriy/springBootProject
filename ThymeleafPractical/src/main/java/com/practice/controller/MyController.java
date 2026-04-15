18cac39b4e0df2868c1fe5819a0543d43345ff84
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-04-15 15:50:37 +0530

    Iteration in Thymeleaf | how to loop in thymeleaf | spring boot.

--- ThymeleafPractical/src/main/java/com/practice/controller/MyController.java
+++ ThymeleafPractical/src/main/java/com/practice/controller/MyController.java
@@ -1,30 +1,34 @@
 package com.practice.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 
 @Controller
 public class MyController {
 
 	@RequestMapping(value = "/about", method = RequestMethod.GET)
 	public String about(Model model) {
-		
 		System.out.println("Inside about handler...");
 		// putting data in model...
-		model.addAttribute("name","dharam");
-		model.addAttribute("currentDate",new Date().toLocaleString());
-		
-		// example...
-//		String nameString="anc";
-//		nameString.toUpperCase()model;
-		
+		model.addAttribute("name", "dharam");
+		model.addAttribute("currentDate", new Date().toLocaleString());
 		return "about";
 		// about.html
 	}
-	
+
+	// handling iteration...
+	@GetMapping("/example-loop")
+	public String iterateHandler(Model model) {
+		// create a list, set collection...
+		List<String> names = List.of("ankit", "laxmi", "karan", "john");
+		model.addAttribute("names", names);
+		return "iterate";
+	}
+
 }
